52fccc1ba080c6aeccacb292687d7b75c2787a29
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-07-13 16:40:15 -0400

    cleaned up code, added new displayOutput, added clear function, initialized array for calculation functions

--- calculator.js
+++ calculator.js
@@ -9,13 +9,17 @@
 //add accessibility sound feedback
 
 const container = document.getElementById("container");
-const input = document.createElement("textarea");
+const displayInput = document.createElement("textarea");
+const displayOutput = document.createElement("textarea");
 const keypad = document.createElement("div");
 
-input.id = "input";
-input.style.resize = "none";
+displayInput.id = "displayInput";
+displayInput.style.resize = "none";
+displayOutput.id = "displayOutput";
+displayOutput.style.resize = "none";
 keypad.id = "keypad";
-container.appendChild(input);
+container.appendChild(displayInput);
+container.appendChild(displayOutput);
 container.appendChild(keypad);
 
 for (let i = 0; i < 17; i++) {
@@ -29,131 +33,110 @@ for (let i = 0; i < 17; i++) {
       btn.textContent = "Delete";
       btn.id = `btnDelete`;
       btn.setAttribute("data-key", `Delete`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
+      window.addEventListener("keyup", clear);
+      window.removeEventListener("keydown", getKeypress);
       break;
     case "1":
       btn.textContent = "/";
       btn.id = `btnDivide`;
       btn.setAttribute("data-key", `NumpadDivide`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "2":
       btn.textContent = "*";
       btn.id = `btnMultiply`;
       btn.setAttribute("data-key", `NumpadMultiply`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "3":
       btn.textContent = "-";
       btn.id = `btnSubtract`;
       btn.setAttribute("data-key", `NumpadSubtract`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "4":
       btn.textContent = "7";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       btn.value = 7;
       break;
     case "5":
       btn.textContent = "8";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "6":
       btn.textContent = "9";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "7":
       btn.textContent = "+";
       btn.id = `btnAdd`;
       btn.setAttribute("data-key", `NumpadAdd`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "8":
       btn.textContent = "4";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "9":
       btn.textContent = "5";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "10":
       btn.textContent = "6";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "11":
       btn.textContent = "=";
       btn.id = `btnEqual`;
       btn.setAttribute("data-key", `NumpadEnter`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "12":
       btn.textContent = "1";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "13":
       btn.textContent = "2";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "14":
       btn.textContent = "3";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "15":
       btn.textContent = "0";
       btn.id = `btn${btn.textContent}`;
       btn.setAttribute("data-key", `Numpad${btn.textContent}`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
     case "16":
       btn.textContent = ".";
       btn.id = `btnDecimal`;
       btn.setAttribute("data-key", `NumpadDecimal`);
-      btn.addEventListener("click", getClick);
-      window.addEventListener("keydown", getKeypress);
       break;
 
     default:
       break;
   }
+  btn.addEventListener("click", getClick);
+  window.addEventListener("keydown", getKeypress);
   keypad.appendChild(btn);
 }
 
-function clear() {
-  console.log(this.textContent);
+function clear(e) {
+  const key = document.querySelector(`.calcBtn[data-key="${e.code}"]`);
+  if (!key) return;
+  if (e.key === "Delete") {
+    displayOutput.textContent = `${e.key}`;
+    setTimeout(() => {
+      displayOutput.textContent = "";
+    }, 1500);
+  } else {
+    return;
+  }
 }
 function divide() {
   console.log(this.textContent);
@@ -173,20 +156,29 @@ function equal() {
 function seven() {
   console.log(this.value, typeof this.value);
 }
+let array = [];
 
 function getKeypress(e) {
   const key = document.querySelector(`.calcBtn[data-key="${e.code}"]`);
   // const secondaryKey = document.querySelector(
   //   `.calcBtn[data-secondary="${e.code}"]`
   // );
+  // const deleteKey = document.querySelector(`.calcBtn[data-key="Delete"]`);
   if (!key) return;
+  if (e.key === `Delete`) {
+    displayInput.textContent = "";
+  } else {
+    array.push(+e.key);
+    console.log(array);
+    displayInput.textContent = `${e.key}`;
+  }
   key.classList.add("selected");
-
-  console.log(e.code);
+  return `${e.key}`;
 }
 function getClick(e) {
-  const key = document.querySelector(`.calcBtn`);
+  // const key = document.querySelector(`.calcBtn`);
   this.classList.add("selected");
+  displayInput.textContent = this.textContent;
   console.log(this.textContent);
 }
 
